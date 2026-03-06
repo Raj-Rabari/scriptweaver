68018dc77363e94cfb9341032389f28a3909c9f1
Raj Rabari <rajrabari8667@gmail.com>
2026-03-06 13:06:23 +0530

    fix: update production api to use custom domain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://13.236.67.40:3000', // Make sure there is no trailing slash!
+  apiUrl: 'http://rrabari.in:3000', // Make sure there is no trailing slash!
 };
